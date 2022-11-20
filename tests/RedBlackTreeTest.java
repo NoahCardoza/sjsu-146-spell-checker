8c717d83aae961f45a876caa5538de40fdab9a2e
NoahCardoza <noahcardoza@gmail.com>
2022-11-19 20:42:36 -0800

    tests: add tests for local word file if applicable and words with definitions

--- tests/RedBlackTreeTest.java
+++ tests/RedBlackTreeTest.java
@@ -1,14 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,13 +44,15 @@ public class RedBlackTreeTest {
 
     @Test
     public void spellCheckerTest() throws IOException, InterruptedException {
-        Cacheable dictionaryString = new Cacheable(
+        CacheableString dictionaryCache = new CacheableString(
                 new File("dictionary.txt"),
                 URI.create("http://www.math.sjsu.edu/~foster/dictionary.txt")
         );
+        dictionaryCache.sync();
 
-        Dictionary dictionary = new Dictionary();
-        dictionary.extend(dictionaryString.get().split("\\s+"));
+
+        StringDictionary dictionary = new StringDictionary();
+        dictionary.extend(dictionaryCache.getContents().split("\\s+"));
         
         String text = Files.readString(Path.of("tests/prologues.txt"));
         HashSet<String> unidentified = new HashSet<>();
@@ -53,21 +63,112 @@ public class RedBlackTreeTest {
             }
         });
 
-        for (String word :
-                unidentified) {
+        for (String word : unidentified) {
             System.out.println(word);
         }
         System.out.printf("%s words could not be found%n", unidentified.size());
     }
 
+    @Test
+    public void localSpellCheckerTest() throws IOException {
+        File localDictionary = new File("/usr/share/dict/words");
+
+        if (!localDictionary.exists()) return;
+
+        StringDictionary dictionary = new StringDictionary();
+        dictionary.extend(Files.readString(localDictionary.toPath()).split("\\s+"));
+
+        String text = Files.readString(Path.of("tests/prologues.txt"));
+        HashSet<String> unidentified = new HashSet<>();
+
+        Arrays.stream(text.split("\\W+")).map(String::toLowerCase).forEach(word -> {
+            if (!dictionary.contains(word)) {
+                unidentified.add(word);
+            }
+        });
+
+        for (String word : unidentified) {
+            System.out.println(word);
+        }
+        System.out.printf("%s words could not be found%n", unidentified.size());
+    }
+
+    @Test
+    public void advancedSpellCheckerTest() throws IOException, ParserConfigurationException, InterruptedException, SAXException {
+        Dictionary dictionary = new Dictionary();
+
+        dictionary.extend(loadDictionaryEntries());
+
+        String text = Files.readString(Path.of("tests/prologues.txt"));
+        HashSet<String> unidentified = new HashSet<>();
+
+        Arrays.stream(text.split("\\W+")).map(String::toLowerCase).forEach(word -> {
+            if (!dictionary.contains(word)) {
+                unidentified.add(word);
+            }
+        });
+
+        for (String word : unidentified) {
+            System.out.println(word);
+        }
+        System.out.printf("%s words could not be found%n", unidentified.size());
+
+        Stream.of("dog", "hut", "tear", "attack", "awesome").forEach(word -> {
+            System.out.printf("> %s%n", word);
+            List<String> definitions = dictionary.define(word).getDefinitions();
+            for (int i = 0; i < definitions.size(); i++) {
+                System.out.printf("| %2d: %s%n", i + 1, definitions.get(i));
+            }
+        });
+    }
+
+    public static ArrayList<DictionaryEntry> loadDictionaryEntries() throws IOException, InterruptedException, ParserConfigurationException, SAXException {
+        ArrayList<DictionaryEntry> dictionaryEntries = new ArrayList<>();
+
+        CacheableZip cacheable = new CacheableZip(
+                new File("dictionary"),
+                URI.create("http://rali.iro.umontreal.ca/GCIDE/new-entries.zip")
+        );
+        cacheable.sync();
+
+        for (char section = 'a'; section <= 'z' ; section++) {
+            File xmlFile = new File(String.format("dictionary/gcide_%s-entries.xml", section));
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("entry");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element el = (Element) node;
+
+                    ArrayList<String> definitions = new ArrayList<>();
+
+                    NodeList innerNodeList = el.getElementsByTagName("def");
+                    for (int j = 0; j < innerNodeList.getLength(); j++) {
+                        Node innerNode = innerNodeList.item(j);
+                        if (innerNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element innerEl = (Element) innerNode;
+                            definitions.add(innerEl.getTextContent());
+                        }
+                    }
+
+                    dictionaryEntries.add(new DictionaryEntry(el.getAttribute("key").strip().toLowerCase(), definitions));
+                }
+            }
+        }
+
+        return dictionaryEntries;
+    }
 
     public static String makeString(RedBlackTree<String> t) {
         class MyVisitor implements Visitor<String> {
             String result = "";
 
             @Override
-            public void visit(Node<String> n) {
-                result = result + n.getKey();
+            public void visit(RBNode<String> n) {
+                result = result + n.getData();
             }
         }
 
@@ -80,9 +181,9 @@ public class RedBlackTreeTest {
         class MyVisitor implements Visitor<String> {
             String result = "";
             @Override
-            public void visit(Node<String> n)
+            public void visit(RBNode<String> n)
             {
-                result = result + "Color: " + n.getColor() + ", Key:" + n.getKey() + " Parent: " + (n.isRoot() ? " " : n.getParent().getKey()) + "\n";
+                result = result + "Color: " + n.getColor() + ", Key:" + n.getData() + " Parent: " + (n.isRoot() ? " " : n.getParent().getData()) + "\n";
             }
         }
 
